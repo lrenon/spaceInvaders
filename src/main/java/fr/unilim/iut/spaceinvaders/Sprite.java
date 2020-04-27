e68e4055dbf94c819d8c40e8707b9d08580305dc
lrenon <leanar@live.fr>
2020-04-27 18:57:31 +0200

    refactoring Sprite

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/Sprite.java
@@ -0,0 +1,68 @@
+package fr.unilim.iut.spaceinvaders;
+
+public abstract class Sprite {
+
+	protected Position origine;
+	protected Dimension dimension;
+	protected int vitesse;
+
+	// Constructeurs
+	public Sprite(Dimension dimension, Position origine, int vitesse) {
+		super();
+		this.dimension = dimension;
+		this.origine = origine;
+		this.vitesse = vitesse;
+	}
+
+	public Sprite() {
+		super();
+	}
+
+	// Getters et setters
+	public int abscisseLaPlusAGauche() {
+		return this.origine.abscisse();
+	}
+
+	protected int ordonneeLaPlusHaute() {
+		return this.origine.ordonnee();
+	}
+
+	protected int ordonneeLaPlusBasse() {
+		return ordonneeLaPlusHaute() - this.dimension.hauteur() + 1;
+	}
+
+	public int abscisseLaPlusADroite() {
+		return this.origine.abscisse() + this.dimension.longueur() - 1;
+	}
+
+	public Dimension dimension() {
+		return this.dimension;
+	}
+
+	// Méthodes
+	public boolean occupeLaPosition(int x, int y) {
+		return (estAbscisseCouverte(x) && estOrdonneeCouverte(y));
+	}
+
+	private boolean estOrdonneeCouverte(int y) {
+		return (ordonneeLaPlusBasse() <= y) && (y <= ordonneeLaPlusHaute());
+	}
+
+	private boolean estAbscisseCouverte(int x) {
+		return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
+	}
+
+	public void seDeplacerVersLaDroite() {
+		this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
+	}
+
+	public void seDeplacerVersLaGauche() {
+		this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
+	}
+
+	public void positionner(int x, int y) {
+		this.origine.changerAbscisse(x);
+		this.origine.changerOrdonnee(y);
+	}
+
+}
--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -1,75 +1,10 @@
 package fr.unilim.iut.spaceinvaders;
 
-public class Vaisseau {
-
-	private Position origine;
-	private Dimension dimension;
-	private int vitesse;
+public class Vaisseau extends Sprite {
 
 	// Constructeurs
 	public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
-		this.dimension = dimension;
-		this.origine = positionOrigine;
-		this.vitesse = vitesse;
-	}
-
-	public Vaisseau(Dimension dimension, Position positionOrigine) {
-		this(dimension, positionOrigine, 1);
-	}
-
-	public Vaisseau(int longueur, int hauteur, int x, int y) {
-		this(new Dimension(longueur, hauteur), new Position(x, y));
-	}
-
-	public Vaisseau(int longueur, int hauteur) {
-		this(longueur, hauteur, 0, 0);
-	}
-
-	// Getters et Setters
-	public int abscisseLaPlusAGauche() {
-		return this.origine.abscisse();
-	}
-
-	int ordonneeLaPlusHaute() {
-		return this.origine.ordonnee();
-	}
-
-	int ordonneeLaPlusBasse() {
-		return ordonneeLaPlusHaute() - this.dimension.hauteur() + 1;
-	}
-
-	public int abscisseLaPlusADroite() {
-		return this.origine.abscisse() + this.dimension.longueur() - 1;
-	}
-	
-	public Dimension dimension() {
-		return this.dimension;
-	}
-	
-	// Méthodes
-	public boolean occupeLaPosition(int x, int y) {
-		return (estAbscisseCouverte(x) && estOrdonneeCouverte(y));
-	}
-
-	private boolean estOrdonneeCouverte(int y) {
-		return (ordonneeLaPlusBasse() <= y) && (y <= ordonneeLaPlusHaute());
-	}
-
-	private boolean estAbscisseCouverte(int x) {
-		return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
-	}
-
-	public void seDeplacerVersLaDroite() {
-		this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
-	}
-
-	public void seDeplacerVersLaGauche() {
-		this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
-	}
-
-	public void positionner(int x, int y) {
-		this.origine.changerAbscisse(x);
-		this.origine.changerOrdonnee(y);
+		super(dimension, positionOrigine, vitesse);
 	}
 
 	// toString
